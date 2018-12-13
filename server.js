477928db01956909d84a85be902c41e5ee36c842
Glitch (enchanting-pea) <none>
2018-12-13 14:13:22 +0000

    🚪🚐 Checkpoint ./server.js:2505047/178

--- server.js
+++ server.js
@@ -4,7 +4,6 @@
 // init project
 var express = require('express');
 var app = express();
-var myApp = require('./timestamp');
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -20,8 +19,10 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/timestamp:date_string", function (req, res) {
+  console.log(req.params.date_string);
+  res.json({"time" : req.params.date_string});
+  //res.json({greeting: 'hello API'});
 });
 
 
